2cee346208af9ef5f0a7406604a5b98f22fb6d21
Marcelo Rojas PC <mrojasl@pucp.edu.pe>
2022-07-12 13:32:19 -0500

    filtro

---
+++ src/main/java/com/example/ef_g5/Filter/FilterLogin.java
@@ -0,0 +1,34 @@
+package com.example.ef_g5.Filter;
+
+import com.example.ef_g5.Bean.Empleado;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "FilterLogin")
+public class FilterLogin implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request=(HttpServletRequest) req;
+        HttpServletResponse response=(HttpServletResponse) resp;
+
+        Empleado empleadoLoqueado = (Empleado) request.getSession().getAttribute("empleadoLogueado");
+        if (empleadoLoqueado==null) {
+            response.sendRedirect(request.getContextPath());
+        } else{
+            response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+            response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+            response.setHeader("Expires", "0"); // Proxies.
+            chain.doFilter(request,response);
+        }
+    }
+}
