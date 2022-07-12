c93d195d1293892e8108c13a7dbc700106ef9adf
Marcelo Rojas PC <mrojasl@pucp.edu.pe>
2022-07-12 12:50:38 -0500

    servlets

---
+++ src/main/java/com/example/ef_g5/Servlet/VendedorServlet.java
@@ -0,0 +1,19 @@
+package com.example.ef_g5.Servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "VendedorServlet", value = "/VendedorServlet")
+public class VendedorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
