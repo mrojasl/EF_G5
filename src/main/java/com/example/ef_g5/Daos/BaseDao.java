48ef64aa258588dabf44fc3608141516f7e46de0
Caleb Casapaico <caleb.casapaico@pucp.edu.pe>
2022-07-12 12:09:17 -0500

    BaseDao

---
+++ src/main/java/com/example/ef_g5/Daos/BaseDao.java
@@ -0,0 +1,20 @@
+package com.example.ef_g5.Daos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDao {
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final String user = "root";
+        final String pass = "root";
+        final String url  = "jdbc:mysql://localhost:3306/mysystem4?serverTimezone=America/Lima";
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
